80582ee00f12c16e2d152cb5ffd9ca530b81a651
ninrod <filipe.silva@gmail.com>
2017-11-26 16:37:58 -0200

    add more tests

--- test/evil-surround-test.el
+++ test/evil-surround-test.el
@@ -6,11 +6,16 @@
 (ert-deftest snakecamelfy-evil-operator-test ()
   (ert-info ("basic evil move test")
     (evil-test-buffer
-      "A"
-      ("g~iw")
-      "A"))
+      "[A]B"
+      ("l")
+      "A[B]"))
   (ert-info ("basic text")
     (evil-test-buffer
       "AB"
       ("g~iw")
-      "A_B")))
+      "A_B"))
+  (ert-info ("basic text")
+    (evil-test-buffer
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      ("g~iw")
+      "A_B_C_D_E_F_G_H_I_J_K_L_M_N_O_P_Q_R_S_T_U_V_W_X_Y_Z")))
